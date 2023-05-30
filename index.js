41ea998f93a106ec0bbd39b91fbd88b99017a793
Felipemolina24 <juan_fe59@hotmail.com>
2023-05-30 16:36:49 -0500

    index

--- index.js
+++ index.js
@@ -1,69 +1,58 @@
-const express = require('express')
-require('dotenv').config()
-const { dbConnection } = require('./database/config')
-const cors = require('cors')
-const routes = require('./routes/user')
-const cookieParser = require("cookie-parser")
-const bodyParser = require('body-parser')
-const postRoutes = require('./routes/posts')
-const http = require('http')
-const { Server } = require('socket.io')
-const { log } = require('console')
+const express = require('express');
+require('dotenv').config();
+const { dbConnection } = require('./database/config');
+const cors = require('cors');
+const routes = require('./routes/user');
+const cookieParser = require("cookie-parser");
+const bodyParser = require('body-parser');
+const postRoutes = require('./routes/posts');
+const http = require('http');
+const { Server } = require('socket.io');
 
-//crear Express App
-const app = express()
-const server = http.createServer(app)
+// Crear Express App
+const app = express();
+const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: 'https://playconnect.netlify.app/'
-    }
-})
-
-app.use(bodyParser.json({ limit: "30mb", extended: true }))
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
-
-//Base de datos
-
-dbConnection();
-
-app.use(
-    cors({
-        origin: ["http://localhost:3000", "https://playconnect.netlify.app/"],
-        method: ["GET", "POST"],
-        credentials: true,
-
-    }))
-
-io.on("connection", (socket) => {
-    console.log(socket.id);
-    
-    socket.on('message',  (message) => {
-
-        socket.broadcast.emit('message', {
-            body: message,
-            from: socket.id
-        })
-    })
-
-
+  cors: {
+    origin: 'https://playconnect.netlify.app',
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
 });
 
+// Conexión a la base de datos
+dbConnection();
 
-//Lectura y parseo del body
-app.use(express.json())
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cookieParser());
 
-//Rutas
-app.use('/posts', postRoutes)
-app.use('/user', routes)
+// Configuración de CORS
+app.use(cors({
+  origin: ["http://localhost:3000", "https://playconnect.netlify.app"],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 
+// Lectura y parseo del body
+app.use(express.json());
 
-//puerto 4000
+// Rutas
+app.use('/posts', postRoutes);
+app.use('/user', routes);
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+    
+  socket.on('message', (message) => {
+    socket.broadcast.emit('message', {
+      body: message,
+      from: socket.id
+    });
+  });
+});
+
+// Puerto 4000
 server.listen(process.env.PORT, () => {
-    console.log('Servidor corriendo en puerto', process.env.PORT);
-})
-
-app.use(cookieParser())
-
-app.use("/", routes)
-
-
+  console.log('Servidor corriendo en puerto', process.env.PORT);
+});
